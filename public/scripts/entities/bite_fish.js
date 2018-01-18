64556de69d4a6d60b29d3aad5601d8bb2f13eac7
cjwsstrm <cjwesstrom@gmail.com>
2018-01-18 14:27:09 -0800

    added bite-fish image and bite_fish.js

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,8 @@
+class BiteFish extends Fish {
+
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/bite-fish.gif'; // Set the image
+  }
+
+}
